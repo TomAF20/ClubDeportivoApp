615908bd47e767b910f411445eeb73a48aa87aba
Yair Joaquin Aguero Gamarra <U22219929@utp.edu.pe>
2025-06-30 21:46:32 -0500

    a

--- app/src/main/java/com/example/clubdeportivo/DetallesPartidoActivity.java
+++ app/src/main/java/com/example/clubdeportivo/DetallesPartidoActivity.java
@@ -1,12 +1,51 @@
 package com.example.clubdeportivo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DetallesPartidoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_partido);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_detallesPartido);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Árbitro");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_partidos);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_chat_arbitro) {
+                startActivity(new Intent(this, ChatArbitroActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (id == R.id.nav_historial_arbitro) {
+                startActivity(new Intent(this, HistorialActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (id == R.id.nav_reportar_arbitro) {
+                startActivity(new Intent(this, ReportarProblemaActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            } else if (id == R.id.nav_perfil_arbitro) {
+                startActivity(new Intent(this, PerfilArbitroActivity.class));
+                overridePendingTransition(0, 0);
+                finish();
+                return true;
+            }
+            return id == R.id.nav_partidos;
+        });
     }
 }
