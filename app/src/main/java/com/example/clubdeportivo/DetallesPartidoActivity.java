756cab91ac51ad3cca11307ed3afc9464963b125
Yair Joaquin Aguero Gamarra <U22219929@utp.edu.pe>
2025-06-30 19:27:59 -0500

    +

---
+++ app/src/main/java/com/example/clubdeportivo/DetallesPartidoActivity.java
@@ -0,0 +1,12 @@
+package com.example.clubdeportivo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetallesPartidoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_partido);
+    }
+}
