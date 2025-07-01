f46429480fafd6b68bcf9f42e22a78f238389dda
Yair Joaquin Aguero Gamarra <U22219929@utp.edu.pe>
2025-06-30 22:45:54 -0500

    a

--- app/src/main/java/com/example/clubdeportivo/ArbitroActivity.java
+++ app/src/main/java/com/example/clubdeportivo/ArbitroActivity.java
@@ -52,6 +52,11 @@ public class ArbitroActivity extends AppCompatActivity {
             return id == R.id.nav_partidos;
         });
 
+        Button verMasBtn = findViewById(R.id.btn_ver_mas_partido1);
+        verMasBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ArbitroActivity.this, DetallesPartidoActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
